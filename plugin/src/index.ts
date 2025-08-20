bec6e4d2a0c1d163135dae30afad26f04dec6a19
Aron <Random1996@163.com>
2025-08-20 10:53:50 +0800

    feat: 修复安卓微信入口配置问题

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -6,51 +6,12 @@ import {
 } from "expo/config-plugins";
 
 const withExpoWechat: ConfigPlugin = (config) => {
-  config = withAndroidManifest(config, (config) => {
-    const mainApplication = AndroidConfig.Manifest.getMainApplicationOrThrow(
-      config.modResults
-    );
-    let activities = mainApplication.activity ?? [];
-    let packageName = config.android?.package;
-    let entryActivityIndex = activities.findIndex(
-      (activity) => activity.$["android:name"] === ".wxapi.WXEntryActivity"
-    );
-    let payEntryActivityIndex = activities.findIndex(
-      (activity) => activity.$["android:name"] === ".wxapi.WXPayEntryActivity"
-    );
-
-    if (entryActivityIndex !== -1) {
-      activities.splice(entryActivityIndex, 1);
-    }
-    if (payEntryActivityIndex !== -1) {
-      activities.splice(payEntryActivityIndex, 1);
-    }
-
-    activities.push(
-      {
-        $: {
-          "android:name": ".wxapi.WXEntryActivity",
-          "android:launchMode": "singleTask",
-          "android:exported": "true",
-          "android:theme": "@android:style/Theme.Translucent.NoTitleBar",
-          "android:label": "@string/app_name",
-          "android:taskAffinity": packageName,
-        },
-      },
-      {
-        $: {
-          "android:name": ".wxapi.WXPayEntryActivity",
-          "android:launchMode": "singleTask",
-          "android:exported": "true",
-          "android:theme": "@android:style/Theme.Translucent.NoTitleBar",
-          "android:label": "@string/app_name",
-          "android:taskAffinity": packageName,
-        },
-      }
-    );
-    mainApplication.activity = activities;
-    return config;
-  });
+  // config = withAndroidManifest(config, (config) => {
+  //   const mainApplication = AndroidConfig.Manifest.getMainApplicationOrThrow(
+  //     config.modResults
+  //   );
+  //   return config;
+  // });
 
   config = withInfoPlist(config, (config) => {
     let queriesSchemes = config.modResults.LSApplicationQueriesSchemes ?? [];
